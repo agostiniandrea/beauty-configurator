7090e89e2c9448602f481857c08ec5e897686ee3
Andrea Agostini <a.agostini92@gmail.com>
2018-08-22 15:51:09 +0200

    example of material ui button

--- src/app/React/components/Button/index.jsx
+++ src/app/React/components/Button/index.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Translate from 'react-translate-component';
+import UIButton from '@material-ui/core/Button';
 import './Button.scss';
 
 export default class Button extends Component {
@@ -9,7 +10,12 @@ export default class Button extends Component {
     }
 
     render() {
-        return <Translate content={this.props.text} component="button" onClick={this.props.onClick} />;
+        return (
+            <UIButton variant="contained" color="primary" onClick={this.props.onClick}>
+                <Translate content={this.props.text} />
+            </UIButton>
+        );
+        /* return <Translate content={this.props.text} component="button" onClick={this.props.onClick} />; */
     }
 }
 
